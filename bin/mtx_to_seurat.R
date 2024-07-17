b9cf1098476a1a0ec50d96ec6b9acd149b97d55d
brunapilotto <m.brunapilotto@usp.br>
2024-07-17 16:40:08 -0300

    feat: add sample name

--- bin/mtx_to_seurat.R
+++ bin/mtx_to_seurat.R
@@ -7,8 +7,9 @@ mtx_file      <- args[1]
 barcode_file  <- args[2]
 feature_file  <- args[3]
 out.file      <- args[4]
+sample_name   <- args[5]
 
 expression.matrix <- ReadMtx(mtx = mtx_file, features = feature_file, cells = barcode_file)
-seurat.object <- CreateSeuratObject(counts = expression.matrix)
+seurat.object <- CreateSeuratObject(counts = expression.matrix, project = sample_name)
 
 saveRDS(seurat.object, file = out.file)
