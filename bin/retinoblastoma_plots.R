4739bb71de738bcb51ebe45236cc822c0a8ceaa5
brunapilotto <m.brunapilotto@usp.br>
2024-09-11 21:13:29 -0300

    reafactor

--- bin/retinoblastoma_plots.R
+++ bin/retinoblastoma_plots.R
@@ -17,30 +17,35 @@ library(AUCell)
 args <- commandArgs(trailingOnly=TRUE)
 seurat_object <- args[1]
 sample_name <- args[2]
-retinoblastoma_json <- args[3]
+signatures_json <- args[3]
 
 seurat_object <- readRDS(seurat_object)
-retinoblastoma_cell_types <- fromJSON(retinoblastoma_json)
+signatures <- fromJSON(signatures_json)
 
-########## RB cell types ##########
+if (sample_name == "Integrated") {
+  counts <- GetAssayData(seurat_object, layer = "data")
+} else {
+  counts <- GetAssayData(seurat_object, layer = "counts")
+}
+ranking <- AUCell_buildRankings(counts)
+genes_in_seurat <- rownames(seurat_object)
+
+########## RB cells ##########
 
 genes_list <- list()
-
 cell_types <- c(
-  "Glial",
-  "Cancer-associated_Fibroblasts",
-  "Rod-like",
-  "Cone-like",
-  "Cone_precursor-like",
-  "MKI67+Cone_precursor",
-  "Neural_cells"
+  "Retinoblastoma",
+  "B cells",
+  "T cells",
+  "NK cells",
+  "CAF",
+  "Progenitors"
 )
 
 for (category in cell_types) {
-  genes_list[[category]] <- retinoblastoma_cell_types[[category]]$geneSymbols
+  genes_list[[category]] <- signatures[[category]]$geneSymbols
 }
 
-genes_in_seurat <- rownames(seurat_object)
 filtered_genes_list <- list()
 for (category in names(genes_list)) {
   genes_present <- intersect(genes_list[[category]], genes_in_seurat)
@@ -48,17 +53,62 @@ for (category in names(genes_list)) {
   
   if (percentage_present > 20) {
     filtered_genes_list[[category]] <- genes_list[[category]]
+  } else {
+    print(paste(category, "hallmark not present in the dataset"))
   }
 }
 rm(genes_list)
 
-if (sample_name == "Integrated") {
-  counts <- GetAssayData(seurat_object, layer = "data")
-} else {
-  counts <- GetAssayData(seurat_object, layer = "counts")
+if (!"CellType" %in% colnames(seurat_object@meta.data)) {
+  seurat_object$CellType <- NA
 }
 
-ranking <- AUCell_buildRankings(counts)
+for (category in names(filtered_genes_list)) {
+  cell_AUC <- AUCell_calcAUC(filtered_genes_list[[category]], ranking)
+  cell_assigment <- AUCell_exploreThresholds(cell_AUC, plotHist = TRUE, assign=TRUE)
+  seurat_object$CellType <- ifelse(colnames(seurat_object) %in% cell_assigment$geneSet$assignment, 
+                              gsub("_", " ", category), 
+                              seurat_object$CellType)
+}
+
+cells_type_plot <- DimPlot(seurat_object, label = TRUE, repel = TRUE, 
+                            label.size = 3, group.by = "CellType") +
+                            ggtitle("Cell types")
+ggsave(filename = "cells_type_plot.png", plot = cells_type_plot, dpi = 300, height=7, width=10, units = "in")
+
+cell_type_counts_df <- as.data.frame(table(seurat_object$CellType))
+colnames(cell_type_counts_df) <- c("CellType", "Count")
+write.table(cell_type_counts_df, "cell_type_counts.txt", sep="\t", quote = F, row.names = F)
+
+########## RB cell types ##########
+
+genes_list <- list()
+cell_in_retinoblastoma <- c(
+  "Glial",
+  "CAF",
+  "Rod-like",
+  "Cone-like",
+  "Cone_precursor-like",
+  "MKI67+Cone_precursor",
+  "Neural_cells"
+)
+
+for (category in cell_in_retinoblastoma) {
+  genes_list[[category]] <- signatures[[category]]$geneSymbols
+}
+
+filtered_genes_list <- list()
+for (category in names(genes_list)) {
+  genes_present <- intersect(genes_list[[category]], genes_in_seurat)
+  percentage_present <- length(genes_present) / length(genes_list[[category]]) * 100
+  
+  if (percentage_present > 20) {
+    filtered_genes_list[[category]] <- genes_list[[category]]
+  } else {
+    print(paste(category, "hallmark not present in the dataset"))
+  }
+}
+rm(genes_list)
 
 if (!"RBcells" %in% colnames(seurat_object@meta.data)) {
   seurat_object$RBcells <- NA
@@ -77,20 +127,30 @@ rb_cells_plot <- DimPlot(seurat_object, label = TRUE, repel = TRUE,
                             ggtitle("Cell types in human RB")
 ggsave(filename = "rb_cells_plot.png", plot = rb_cells_plot, dpi = 300, height=7, width=10, units = "in")
 
-########## RB cells ##########
+cell_type_in_rb_counts <- table(seurat_object$RBcells)
+cell_type_counts_df <- as.data.frame(cell_type_in_rb_counts)
+colnames(cell_type_counts_df) <- c("CellType", "Count")
+write.table(cell_type_counts_df, "cell_type_in_rb_counts.txt", sep="\t", quote = F, row.names = F)
 
-genes_present <- intersect(retinoblastoma_cell_types$Retinoblastoma$geneSymbols, genes_in_seurat)
-percentage_present <- length(genes_present) / length(retinoblastoma_cell_types$Retinoblastoma$geneSymbols) * 100
+######### EMTome #########
+
+genes_present <- intersect(signatures$EMT$geneSymbols, genes_in_seurat)
+percentage_present <- length(genes_present) / length(signatures$EMT$geneSymbols) * 100
 if (percentage_present > 20) {
-  cell_AUC <- AUCell_calcAUC(retinoblastoma_cell_types$Retinoblastoma$geneSymbols, ranking)
+  cell_AUC <- AUCell_calcAUC(signatures$EMT$geneSymbols, ranking)
   cell_assigment <- AUCell_exploreThresholds(cell_AUC, plotHist = TRUE, assign=TRUE)
-  seurat_object$RB <- ifelse(colnames(seurat_object) %in% cell_assigment$geneSet$assignment, 
-                              "RB", 
+  seurat_object$EMT <- ifelse(colnames(seurat_object) %in% cell_assigment$geneSet$assignment, 
+                              "EMT", 
                               NA)
-  RB_plot <- DimPlot(seurat_object, repel = TRUE, 
-                            label.size = 3, group.by = "RB") +
-                            ggtitle("Retinoblastoma cells")
-  ggsave(filename = "RB_plot.png", plot = RB_plot, dpi = 300, height=5, units = "in")
+  emt_plot <- DimPlot(seurat_object, repel = TRUE, 
+                            label.size = 3, group.by = "EMT") +
+                            ggtitle("EMT")
+  ggsave(filename = "emt_plot.png", plot = emt_plot, dpi = 300, height=5, units = "in")
+
+  EMT_counts <- table(seurat_object$EMT)
+  EMT_counts_df <- as.data.frame(EMT_counts)
+  colnames(EMT_counts_df) <- c("CellType", "Count")
+  write.table(EMT_counts_df, "EMT_counts.txt", sep="\t", quote = F, row.names = F)
 } else {
-  print("Retinoblastoma cells not present in the dataset")
+  print("EMT hallmark not present in the dataset")
 }
